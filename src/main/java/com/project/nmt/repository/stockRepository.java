4c3e2d0ef0da47720f4645efe5088620f8101d81
Bogeun <pky2892@gmail.com>
2021-07-19 16:15:24 +0900

    지금은 필요없는 것 delete

--- src/main/java/com/project/nmt/repository/stockRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.nmt.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.project.nmt.model.StockInfo;
-
-public interface stockRepository extends JpaRepository<StockInfo, Long> {
-
-}
