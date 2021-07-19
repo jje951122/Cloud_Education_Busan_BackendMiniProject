d0f75bee913e24d4febab730b914ca71606c7b2e
yeseul111 <jinys99@naver.com>
2021-07-19 15:51:14 +0900

    브랜치 추가

--- src/main/java/com/project/nmt/NmtApplication.java
+++ src/main/java/com/project/nmt/NmtApplication.java
@@ -17,5 +17,6 @@ public class NmtApplication implements CommandLineRunner {
         // 현재 우리 db는 인메모리 db인 h2이기 때문에, 내용이 계속 날라가게 되어있습니다.
         // 때문에 여따가 데이터를 넣어주는 코드를 박아두고 개발하시면, 서버를 다시 띄워도 얘가 넣어줍니다.
         // application.properties를 수정하셔서 서버를 꺼도 날라가지 않는 db를 사용하셔도 됩니다.
+    	// 예슬
     }
 }
