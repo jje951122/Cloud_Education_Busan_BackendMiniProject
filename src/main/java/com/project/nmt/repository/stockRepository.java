6f71b86207e287ec5a09f8661ab27de969d376c0
jje951122 <jje951122@naver.com>
2021-07-19 16:07:07 +0900

    장주언 - data setting 하는중

---
+++ src/main/java/com/project/nmt/repository/stockRepository.java
@@ -0,0 +1,9 @@
+package com.project.nmt.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.project.nmt.model.StockInfo;
+
+public interface stockRepository extends JpaRepository<StockInfo, Long>{
+	
+}
