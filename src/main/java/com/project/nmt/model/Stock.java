f874e4d1e432a1f8dd91c97bb92da53ed5fcaa76
Bogeun <pky2892@gmail.com>
2021-07-20 11:07:45 +0900

    Stock 테이블 칼럼 추가

--- src/main/java/com/project/nmt/model/Stock.java
+++ src/main/java/com/project/nmt/model/Stock.java
@@ -15,11 +15,12 @@ public class Stock {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    private String name;   // 종목 이름
-    private int quantity;  // 종목 잔여 수량
+    private String name;     // 종목 이름
+    private String keyword;  // 검색 키워드
+    private int quantity;    // 종목 잔여 수량
 
-//    @OneToMany(mappedBy = "stock")
-//    private final List<StockInfo> stockInfos = new ArrayList<>();
+    @OneToMany(mappedBy = "stock")
+    private final List<StockInfo> stockInfos = new ArrayList<>();
 
 
     @Builder
