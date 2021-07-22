12351c5bb591feb26625f24a15707ffa024c9658
Bogeun <pky2892@gmail.com>
2021-07-22 09:35:58 +0900

    모든 브랜치 merge

--- src/main/java/com/project/nmt/repository/stockRepository.java
+++ src/main/java/com/project/nmt/repository/stockRepository.java
@@ -1,6 +1,5 @@
 package com.project.nmt.repository;
 
-
 import com.project.nmt.model.Stock;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -11,5 +10,5 @@ public interface StockRepository extends JpaRepository<Stock, Long> {
 
     @Query("select s.keyword from Stock s")
     List<String> findAllStockKeyword();
-}
 
+}
